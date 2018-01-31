184e92e8f4aac6669b9169a96ac13aca72a7bc9d
davidmmanning617 <36010767+davidmmanning617@users.noreply.github.com>
2018-01-31 13:30:13 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,2 @@
-ldclient.on('ready', function() {
-  let d = new Date();
-  var showFeature = ldclient.variation("know-todays-date", false);
-  if (showFeature) {
-    document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
-  } else {
-    document.body.innerHTML = "<h1>Not today, friend."</h1>"
-  }
-});
+let d = new Date();
+document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
