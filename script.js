c2cf728682b2ca4679428bc2c5fc35e8175ff1e5
davidmmanning617 <36010767+davidmmanning617@users.noreply.github.com>
2018-01-31 15:32:24 -0800

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,5 @@ ldclient.on('ready', function() {
     let d = new Date();
     document.body.innerHTML = "<h1>Today's date is " + d + "</h1>";
   } else {
-    document.body.innerHTML = "<h1>Not today, friend."</h1>";
   }
 });
