106b38b13eb1a367a0f98c3d0eb911f9815dc61b
davidmmanning617 <36010767+davidmmanning617@users.noreply.github.com>
2018-01-31 13:27:37 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var ldclient = LDClient.initialize('5a710e426040390af06d0b14', user);
 let d = new Date();
 ldclient.on('ready', function() {
-  var showFeature = ldclient.variation("your.flag.key", false);
+  var showFeature = ldclient.variation("know-todays-date", false);
   if (showFeature) {
     document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
   } else {
