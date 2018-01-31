849279fa9f66d847a7bd0e0f13ea3e85ea6984c4
davidmmanning617 <36010767+davidmmanning617@users.noreply.github.com>
2018-01-31 13:26:18 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
+var ldclient = LDClient.initialize('5a710e426040390af06d0b14', user);
 let d = new Date();
-document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+ldclient.on('ready', function() {
+  var showFeature = ldclient.variation("your.flag.key", false);
+  if (showFeature) {
+    document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+  } else {
+    document.body.innerHTML = "<h1>Not today, friend."</h1>"
+  }
+});
+
+
